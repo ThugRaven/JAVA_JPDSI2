fda532d702c1609f213b8798dcd141197c495029
ThugRaven <kamilo1715@wp.pl>
2021-10-13 20:38:14 +0200

    Add Lab 3

---
+++ Lab3/calc_cred_2/src/main/java/calc/CalcCredBB.java
@@ -0,0 +1,82 @@
+package calc;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+@Named
+@RequestScoped
+//@SessionScoped
+public class CalcCredBB {
+	private String amount;
+	private String years;
+	private String interest;
+	private Double result;
+
+	public String getAmount() {
+		return amount;
+	}
+
+	public void setAmount(String amount) {
+		this.amount = amount;
+	}
+
+	public String getYears() {
+		return years;
+	}
+
+	public void setYears(String years) {
+		this.years = years;
+	}
+
+	public String getInterest() {
+		return interest;
+	}
+
+	public void setInterest(String interest) {
+		this.interest = interest;
+	}
+
+	public Double getResult() {
+		return result;
+	}
+
+	@Inject
+	FacesContext ctx;
+
+	public boolean doTheMath() {
+		try {
+			double amount = Double.parseDouble(this.amount);
+			double years = Double.parseDouble(this.years);
+			double interest = Double.parseDouble(this.interest);
+
+			Double monthly = amount / (years * 12);
+			Double percent = monthly * (interest / 100);
+			result = monthly + percent;
+
+			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Operacja wykonana poprawnie", null));
+			return true;
+		} catch (Exception e) {
+			ctx.addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "B³¹d podczas przetwarzania parametrów", null));
+			return false;
+		}
+	}
+
+	public String calc() {
+		if (doTheMath()) {
+			return "showresult";
+		}
+		return null;
+	}
+
+	public String calc_AJAX() {
+		if (doTheMath()) {
+			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Wynik: " + result, null));
+		}
+		return null;
+	}
+}
